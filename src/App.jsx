2b52208b05660764cb60cf134cb165438f299b35
John Romanazzi <jdr84@njit.edu>
2026-03-05 16:13:40 -0500

    Add customers frontend with search, drawer details, edit, deactivate, and create

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
 import Customers from "./pages/Customers.jsx";
-import CustomerDetails from "./pages/CustomerDetails.jsx";
-import Films from "./pages/Films.jsx";   // <-- add this
+import Films from "./pages/Films.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/customers" element={<Customers />} />
-      <Route path="/customers/:id" element={<CustomerDetails />} />
-      <Route path="/films" element={<Films />} />   {/* <-- add this */}
+      <Route path="/films" element={<Films />} />
     </Routes>
   );
 }
